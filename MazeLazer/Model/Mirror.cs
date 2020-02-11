cb28c72620bb017808aa1c62a8e1106b7064c3dc
Boeschen, Tim <tim.boeschen@pioneer.com>
2020-02-11 11:00:21 -0600

    Removed unused statements.

--- MazeLazer/Model/Mirror.cs
+++ MazeLazer/Model/Mirror.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-
-namespace MazeLaser.Model
+﻿namespace MazeLaser.Model
 {
     public enum Lean { Right, Left }
     public enum ReflectiveSide { Right, Left, Both }
